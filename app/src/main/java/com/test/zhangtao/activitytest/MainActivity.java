153c1423cf5196eb42b3dfe84bf739c6c49fcdec
z13538657403 <742532915@qq.com>
2017-01-03 18:00:40 +0800

    Initial commit

---
+++ app/src/main/java/com/test/zhangtao/activitytest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.test.zhangtao.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangtao on 17/1/3.
+ */
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
